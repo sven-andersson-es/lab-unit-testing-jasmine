286a18dc3906d4dfbc2a1845355cc2574a993930
Sven Andersson <sven@andersson.es>
2024-09-28 13:04:09 +0200

    create tests for divide function

--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,22 @@
-// UNCOMMENT THE CODE BELOW TO START
-
-/*
-
 describe("Iteration 2", () => {
     describe("Function - divide", () => {
         
         // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
-
+        it("should have the function divide defined", () => {
+            expect(typeof divide).toBe("function")
+        });
+        it('should take two numbers as arguments', () => {
+            expect(divide.length).toBe(2);
+        });
+        it('should return the division of two numbers', () => {
+            expect(divide(4,2)).toBe(2);
+            expect(divide(100,4)).toBe(25);
+        });
+        it('should return undefined if any of the two arguments is not provided', () => {
+            expect(divide()).toBe(undefined);
+            expect(divide(1)).toBe(undefined);
+            expect(divide(undefined,2)).toBe(undefined);
+        });
     })    
 })
 
-*/
