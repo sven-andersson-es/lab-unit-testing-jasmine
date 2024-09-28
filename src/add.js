6b12abc5bb6857924282755902f73b20e734490f
Sven Andersson <sven@andersson.es>
2024-09-28 13:15:06 +0200

    fix for add function type error

--- src/add.js
+++ src/add.js
@@ -1,7 +1,7 @@
 function add(numOne, numTwo) {
 	if (typeof numOne === "number" && typeof numTwo === "number") {
 		return numOne + numTwo;
-	} else if (numOne === undefined || numTwo == = undefined) {
+	} else if (numOne === undefined || numTwo === undefined) {
 		return undefined;
 	} else {
 		return undefined;
