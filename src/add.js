a47eadc4e470f23afc568d77992616e3f80d0e7b
Sven Andersson <sven@andersson.es>
2024-09-28 12:51:25 +0200

    create tests for add function

--- src/add.js
+++ src/add.js
@@ -1,7 +1,9 @@
-function add (numOne, numTwo) {
-    if (numOne == undefined || numTwo == undefined) {
-        return undefined;
-    }
-
-    return numOne + numTwo;
+function add(numOne, numTwo) {
+	if (typeof numOne === "number" && typeof numTwo === "number") {
+		return numOne + numTwo;
+	} else if (numOne === undefined || numTwo === undefined) {
+		return undefined;
+	} else {
+		return undefined;
+	}
 }
